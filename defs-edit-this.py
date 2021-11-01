8a989c71674b999f0cf2913d9121694687b17cb8
Juan J Guirao Garcia <juan.jose.guirao@gmail.com>
2021-11-01 21:46:05 +0000

    Defs - version for github
    The original version (defs.py) contains my personal Oanda API Key and Account number and is, therefore, included in gitignore and not pushed to github.

---
+++ defs-edit-this.py
@@ -0,0 +1,14 @@
+# =========
+# IMPORTANT
+# =========
+
+# Rename this file to defs.py
+# enter your OANDA API_KEY and your OANDA Account ID below.
+
+API_KEY = "xxxxxxx"
+ACCOUNT_ID = "xxxxxxx"
+OANDA_URL = 'https://api-fxpractice.oanda.com/v3'
+
+SECURE_HEADER = {
+        'Authorization': f'Bearer {API_KEY}'
+}
